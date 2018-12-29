f6e531398206d5476e11176a7b1a93f05753a33c
Nikola Cvitaš <nikola.cvitas@btb.hr>
2018-12-29 23:51:05 +0100

    First commit.

---
+++ spring-demo-annotations/src/hr/nikola/spring/Main.java
@@ -0,0 +1,21 @@
+package hr.nikola.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+
+		
+		
+		
+
+	}
+
+}
